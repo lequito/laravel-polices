e0a2b7a875de0e3b3c7fcc93a096ea9de9e89306
Lekito <alexdossantosdias1212@gmail.com>
2025-03-07 18:59:09 -0300

    seed for user and posts

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,14 +4,27 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class PostSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
+class PostSeeder extends Seeder{
+
+    public function run(): void{
+        
+        $roles = ['admin', 'moderador', 'visitor'];
+        $posts = [];
+
+        $user_id = 1;
+
+        foreach($roles as $role){
+            $posts[] = [
+                'user_id'    => $user_id++,
+                'title'      => "Post do usuário $role",
+                'content'    => "Conteúdo do post do usuário $role",
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        }
+
+        DB::table('posts')->insert($posts);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,14 +4,30 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
+    public function run(): void{
+        
+        $roles = ['admin', 'moderador', 'visitor'];
+
+        $users = [];
+
+        foreach($roles as $role){
+            $user[] = [
+                'name'       => $role,
+                'email'      => "$role@mail.com",
+                'password'   => bcrypt('Aa123456'),
+                'role'       => $role,
+                'created_at' => now(),
+                'updated_at' => now()
+            ];
+        }
+
+        DB::table('users')->insert($user);
     }
 }
